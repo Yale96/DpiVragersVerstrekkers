5658d840132ae962144c849fdd991dbf1f75aba5
Yale96 <yannickvanleeuwen@i-lion.nl>
2018-05-24 09:47:36 +0200

    Added classes

--- VragersVerstrekkers/src/ConsumersProducers/producer.java
+++ VragersVerstrekkers/src/ConsumersProducers/producer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package publishsubscribetutorial;
+package ConsumersProducers;
 
 /**
  *
@@ -15,23 +15,35 @@ import javax.jms.*;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-public class consumer {
+public class producer {
 
-    // URL of the JMS server
     private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
 
-    // Name of the topic from which we will receive messages from = " testt"
     public static void main(String[] args) throws JMSException {
-        // Getting JMS connection from the server
+
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
 
+        // JMS messages are sent and received using a Session. We will
+        // create here a non-transactional session object. If you want
+        // to use transactions you should set the first parameter to 'true'
         Session session = connection.createSession(false,
                 Session.AUTO_ACKNOWLEDGE);
 
         Topic topic = session.createTopic("testt");
 
+        MessageProducer producer = session.createProducer(topic);
+
+        // We will send a small text message saying 'Hello'
+
+        TextMessage message = session.createTextMessage();
+
+        message.setText("Yes");
+        // Here we are sending the message!
+        producer.send(message);
+        System.out.println("Sent message '" + message.getText() + "'");
+        
         MessageConsumer consumer = session.createConsumer(topic);
 
         MessageListener listner = new MessageListener() {
@@ -42,18 +54,6 @@ public class consumer {
                         TextMessage textMessage = (TextMessage) message;
                         System.out.println("Received message  "
                                 + textMessage.getText() + "'");
-                        if (textMessage.getText().equals("Yes")) {
-                            String s = "Debug";
-                            MessageProducer producer = session.createProducer(topic);
-
-                            // We will send a small text message saying 'Hello'
-                            TextMessage messageReply = session.createTextMessage();
-
-                            messageReply.setText("Received");
-                            // Here we are sending the message!
-                            producer.send(message);
-                            System.out.println("Sent message '" + messageReply.getText() + "'");
-                        }
                     }
                 } catch (JMSException e) {
                     System.out.println("Caught:" + e);
